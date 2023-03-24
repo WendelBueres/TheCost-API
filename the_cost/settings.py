823fb5c4be32c08f19b1dc54210f45ecbd5be64e
Wendel Bueres <wendelbueres@gmail.com>
2023-03-24 17:04:52 -0300

    fix: :bug: secret_key

--- the_cost/settings.py
+++ the_cost/settings.py
@@ -16,6 +16,8 @@ import dotenv
 import os
 from django.core.management.utils import get_random_secret_key
 
+dotenv.load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
