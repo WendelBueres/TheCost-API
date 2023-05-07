6d4db1d443ae028458d7828754df252a08ee17ff
Wendel Bueres <wendelbueres@gmail.com>
2023-05-07 00:12:01 -0300

    feat: :sparkles: adicionada url token refresh, verify e kill.

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .views import UserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenBlacklistView, TokenRefreshView
+from .views import *
 
 urlpatterns = [
     path("users/login/", TokenObtainPairView.as_view()),
+    path("users/login/refresh/", TokenRefreshView.as_view()),
+    path("users/login/verify/", TokenVerifyView.as_view()),
+    path("users/login/killtoken/", TokenBlacklistView.as_view()),
     path("users/", UserView.as_view()),
 ]
